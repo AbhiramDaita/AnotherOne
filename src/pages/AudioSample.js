94b8ad35a1b278d9eb1c0000bec1a9735373cfbf
Abhiram <69247454+AbhiramDaita@users.noreply.github.com>
2024-03-01 14:18:49 +0530

    Update AudioSample.js

--- src/pages/AudioSample.js
+++ src/pages/AudioSample.js
@@ -1,19 +1,28 @@
+
 import '../App.css';
 
 import Card from '../components/card';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import Header from '../components/header';
 
 export default function AudioSample(){
     return(
+        <div>
+            <Header/>
         <div className="audioHolder">
-            <div className="note">
-                Please make sure you are in a quiet environment and keep your device around 20 cm from your face.
+            <div>
+            <p className='backgroundText'>Upload</p>
+                <div className="note">
+                    <ErrorOutlineIcon/>
+                    <p className='hello'> Please make sure you are in a quiet environment and keep your device around 20 cm from your face.</p>
+                </div>
+                <Card title="Breathing(shallow)" cap="Breathe fast 5 times"/>
+                <Card title="Cough(shallow)" cap="Cough mildly 3 times"/>
+                <Card title="Vowel /a/" cap="Say /a/ as in made and sustain as long as possible"/>
+                <Card title="Vowel /e/" cap="Say /e/ as in beet and sustain as long as possible"/>
+                <Card title="Vowel /o/" cap="Say /o/ as in cool and sustain as long as possible"/>
             </div>
-            <Card title="Breathing(shallow)" cap="Breathe fast 5 times"/>
-            <Card title="Cough(shallow)" cap="Cough mildly 3 times"/>
-            <Card title="Vowel /a/" cap="Say /a/ as in made and sustain as long as possible"/>
-            <Card title="Vowel /e/" cap="Say /e/ as in beet and sustain as long as possible"/>
-            <Card title="Vowel /o/" cap="Say /o/ as in cool and sustain as long as possible"/>
-            
+        </div>
         </div>
     )
 }
